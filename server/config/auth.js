2056d177fb8717521b20a1bf9a22c98afcd7c829
Jon Balon <development@jonbalon.com>
2017-01-02 18:32:55 -0500

    Moved twitter callback url into env variables

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,7 +3,7 @@ const auth = {
   TWITTER: {
     CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
     CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET,
-    CALLBACK_URL: 'http://127.0.0.1:8080/api/auth/twitter/callback',
+    CALLBACK_URL: process.env.TWITTER_CALLBACK_URL,
   },
   KEY: {
     SESSION_SECRET_KEY: process.env.SESSION_SECRET_KEY,
