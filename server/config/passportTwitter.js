aa1a9b6e7f8f338a04bef899eacaea32efea0f7f
Jon Balon <development@jonbalon.com>
2017-01-02 00:25:39 -0500

    Started work on twitter passport strategy

---
+++ server/config/passportTwitter.js
@@ -0,0 +1,73 @@
+// Dependencies.
+const passport = require('passport'); // Passport.
+const TwitterStrategy = require('passport-twitter'); // Twitter Passport Strategy.
+const rfr = require('rfr'); // Root-relative paths.
+const mongoose = require('mongoose'); // MongoDB database driver.
+
+// Database config file.
+const DBCONFIG = rfr('/server/config/database');
+
+// Connect to the database.
+mongoose.Promise = global.Promise;
+mongoose.createConnection(DBCONFIG.CONNSTRING);
+
+// Models.
+const User = rfr('/server/models/User');
+
+// Twitter API keys.
+const auth = rfr('/server/config/auth');
+
+// Configure the Twitter Strategy.
+passport.use(new TwitterStrategy({
+  consumerKey: auth.TWITTER.CONSUMER_KEY,
+  consumerSecret: auth.TWITTER.CONSUMER_SECRET,
+  callbackURL: auth.TWITTER.CALLBACK_URL,
+}, (token, tokenSecret, profile, done) => {
+  // Search query to find by user ID.
+  const query = { id: profile.id };
+
+  // Make code asynchronous.
+  process.nextTick(() => {
+    // Search the database for a the given User.
+    User.findOne(query, (errFind, resultFind) => {
+      // Error check.
+      if (errFind) { return done(errFind); }
+
+      // If a user was found, log them in.
+      if (resultFind) { return done(null, resultFind); }
+
+      // No user found, create a new user.
+      const newUser = new User();
+      newUser.id = profile.id;
+      newUser.displayName = profile.displayName;
+      newUser.username = profile.username;
+
+      // Save new user into the database.
+      newUser.save((errSave) => {
+        // Error check.
+        if (errSave) { throw errSave; }
+
+        // Return new user.
+        return done(null, newUser);
+      });
+
+      // Keep eslint happy.
+      return true;
+    });
+  });
+}));
+
+// Serialize the user for the session.
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Deserialize the user from the session.
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+// Export the passport.
+module.exports = passport;
