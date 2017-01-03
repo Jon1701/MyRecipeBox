c63aec9469c1bf1e7bab648f6b24550574d36ae0
Jon Balon <development@jonbalon.com>
2017-01-02 22:38:12 -0500

    Added signup responses

--- server/messages/index.js
+++ server/messages/index.js
@@ -12,12 +12,26 @@ const errors = {
     MISSING_TOKEN: response('MISSING_TOKEN', 'Token must be sent in the request header: x-access-token'),
     INVALID_TOKEN: response('INVALID_TOKEN', 'Token is invalid.'),
   },
+  SIGNUP: {
+    MISSING_CREDENTIALS: response('MISSING_CREDENTIALS', 'Both a username and password are required.'),
+    INVALID_USERNAME: response('INVALID_USERNAME', 'Invalid username. Valid characters are letters, numbers, and underscore. Must be between 8 and 25 characters long.'),
+    INVALID_PASSWORD: response('INVALID_PASSWORD', 'Invalid password. Valid characters are letters, numbers, and underscore. Must be between 8 and 50 characters long.'),
+    PASSWORD_MISMATCH: response('PASSWORD_MISMATCH', 'Passwords do not match.'),
+    MISSING_PASSWORD_CONFIRMATION: response('MISSING_PASSWORD_CONFIRMATION', 'Both password and password confirmation are required.'),
+    USER_EXISTS: response('USER_EXISTS', 'User already exists.'),
+  },
+  DB: {
+    DB_ERROR: response('DB_ERROR', 'Unknown database error occurred.'),
+  },
 };
 
 // Success messages.
 const successes = {
   LOGIN: {
     LOGIN_SUCCESS: response('LOGIN_SUCCESS', 'Login successful.'),
+  },
+  SIGNUP: {
+    USER_CREATED: response('USER_CREATED', 'User successfully created.'),
   },
 };
 
