e69339787097d114cec247c71d168a70ae98304b
Jon Balon <development@jonbalon.com>
2017-01-04 18:29:43 -0500

    moved success/error response messages into this file

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -3,8 +3,21 @@ const jwt = require('jsonwebtoken');  // JSON web token.
 const rfr = require('rfr');           // Root relative paths.
 
 // Other dependencies.
-const MSG = rfr('/server/messages/index');// Response error/success messages.
 const JWT_SIGNING_KEY = rfr('/server/config/auth').KEY.JWT_SIGNING_KEY;  // JWT Signing key.
+
+// Success/Error Response messages.
+const MSG = (code) => {
+  const messages = {
+    MISSING_TOKEN: 'Token must be sent in the request header: x-access-token',
+    INVALID_TOKEN: 'Token is invalid.',
+  };
+
+  // Return message as an Object.
+  return {
+    code,
+    message: messages[code],
+  };
+};
 
 // Middleware definition.
 const verifyJWT = (req, res, next) => {
@@ -13,14 +26,14 @@ const verifyJWT = (req, res, next) => {
 
   // If there is no token, return an error as response.
   if (!token) {
-    return res.status(500).json(MSG.ERROR.JWT.MISSING_TOKEN);
+    return res.status(500).json(MSG('MISSING_TOKEN'));
   }
 
   // If a token was provided, decode it using the signing key.
   jwt.verify(token, JWT_SIGNING_KEY, (err, decoded) => {
     // If token is invalid, return error as response.
     if (err) {
-      return res.status(500).json(MSG.ERROR.JWT.INVALID_TOKEN);
+      return res.status(500).json(MSG('INVALID_TOKEN'));
     }
 
     // Token is valid, store decoded token for future use.
