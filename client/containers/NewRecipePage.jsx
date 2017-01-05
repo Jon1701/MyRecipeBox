3000d817fb0b6c966835b4631a6f604213c3e856
Jon Balon <development@jonbalon.com>
2017-01-04 23:15:43 -0500

    Added New Recipe Page

---
+++ client/containers/NewRecipePage.jsx
@@ -0,0 +1,19 @@
+// React.
+import React from 'react';
+
+// React components.
+import NewRecipeWidget from 'components/NewRecipeWidget';
+
+// Component definition.
+const NewRecipePage = () => (
+  <div className="container-page">
+
+    <h1 className="text-center">Create a New Recipe</h1>
+
+    <NewRecipeWidget />
+
+  </div>
+);
+
+// Component export.
+export default NewRecipePage;
