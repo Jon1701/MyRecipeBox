df640dbc4ab6577098055395c3fd5e70f7165184
Jon Balon <development@jonbalon.com>
2017-01-06 19:29:26 -0500

    Added NewRecipeWidget to ViewRecipe page

--- client/containers/ViewRecipePage.jsx
+++ client/containers/ViewRecipePage.jsx
@@ -1,11 +1,16 @@
 // React.
 import React from 'react';
 
+// React Components.
+import NewRecipeWidget from 'components/NewRecipeWidget';
+
 // Component definition.
 const ViewRecipePage = () => (
   <div className="container-page">
 
     <h1 className="text-center">View Recipe</h1>
+
+    <NewRecipeWidget />
 
   </div>
 );
