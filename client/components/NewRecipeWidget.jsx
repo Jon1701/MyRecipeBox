feea8b7f0c406ad0fab8fa513ed67d917f62b6b0
Jon Balon <development@jonbalon.com>
2017-01-08 20:22:53 -0500

    New recipe widget no longer had editing capability

--- client/components/NewRecipeWidget.jsx
+++ client/components/NewRecipeWidget.jsx
@@ -23,8 +23,6 @@ class NewRecipeWidget extends React.Component {
       tagline: '',    // Recipe tagline.
       ingredients: [],  // Array of recipe ingredients.
       instructions: [], // Array of recipe preparation instructions.
-      hideForm: false, // Track form visibility.
-      readOnly: this.props.mode === 'ViewRecipe',  // Form read only or editable
     };
 
     // Bind methods to component instance.
@@ -35,48 +33,6 @@ class NewRecipeWidget extends React.Component {
     this.updateIngredient = this.updateIngredient.bind(this);
     this.updateInstruction = this.updateInstruction.bind(this);
     this.addRemoveFields = this.addRemoveFields.bind(this); // Adds/Remove ingredients/instructions.
-  }
-
-  // Component did mount.
-  componentDidMount() {
-    // Load recipe data if in ViewRecipe mode.
-    if (this.props.mode === 'ViewRecipe') {
-      // Get the recipe id from router props.
-      const recipeID = this.props.params.recipe_id;
-
-      request
-        .get(`/api/get_recipes?recipe_id=${recipeID}`)
-        .then((res) => {
-          switch (res.data.code) {
-            // Recipes found.
-            case 'RECIPE_SEARCH_COMPLETE': {
-              // If no matching recipes were found, throw error for .catch().
-              if (res.data.payload.recipes.length === 0) {
-                throw new Error();
-              }
-
-              // Deconstruct recipe fields.
-              const { title, tagline, ingredients, instructions, username }
-                    = res.data.payload.recipes[0];
-
-              // Store recipe in state.
-              this.setState({ title, tagline, ingredients, instructions, username });
-            }
-              break;
-
-            // Default case: do nothing.
-            default:
-              break;
-          }
-        })
-        .catch(() => {
-          // Hide the form if a recipe could not be found.
-          this.setState({ hideForm: true });
-
-          // Display error message.
-          this.setAlert('FAILURE', 'No recipe was found.');
-        });
-    }
   }
 
   // Method to set the current alert.
@@ -103,12 +59,6 @@ class NewRecipeWidget extends React.Component {
     // Request body.
     const body = { title, tagline, ingredients, instructions };
 
-    // Get the recipe id from router props.
-    const recipeID = this.props.params.recipe_id;
-
-    // If the recipe ID exists, add it to the request body.
-    body['recipe_id'] = recipeID;
-
     // Make a POST request to the API server, send recipe data, and token.
     request
       .post('/api/auth/create_recipe', body, this.props.token)
@@ -133,15 +83,6 @@ class NewRecipeWidget extends React.Component {
             setTimeout(redirect.bind(this), 2000);
             break;
           }
-
-          // Recipe successfully updated.
-          case 'UPDATE_RECIPE_SUCCESS':
-            // Display success message.
-            this.setAlert('SUCCESS', 'Recipe successfully updated.');
-
-            // Set form to read only, since the user has finished editing.
-            this.setState({ readOnly: true });
-            break;
 
           // Default behaviour: do nothing.
           default:
@@ -275,34 +216,6 @@ class NewRecipeWidget extends React.Component {
     return this.setState({ instructions: arrayItems });
   }
 
-  // Helper function to determine if the Edit and Save buttons should be visible.
-  setEditSaveButtonVisibility() {
-    // By default, hide Save and Edit buttons.
-    let hideSaveButton = true;
-    let hideEditButton = true;
-
-    // If viewing a recipe and token is provided.
-    if (this.props.token && this.state.username && this.props.mode === 'ViewRecipe') {
-      // Extract username from token, and recipe.
-      const usernameToken = JSON.parse(atob(this.props.token.split('.')[1])).username;
-      const usernameRecipe = this.state.username;
-
-      /*
-       * If the usernames are the same, then the logged in user wrote the recipe.
-       *  Display edit and save buttons.
-       */
-      if (usernameToken === usernameRecipe) {
-        hideSaveButton = false;
-        hideEditButton = false;
-      }
-    } else if (this.props.mode === 'NewRecipe') {
-      hideEditButton = true;
-      hideSaveButton = false;
-    }
-
-    return { hideSaveButton, hideEditButton };
-  }
-
   // Component render.
   render() {
     // Dynamically render <input/> for each ingredient.
@@ -313,7 +226,6 @@ class NewRecipeWidget extends React.Component {
         type="text"
         value={this.state.ingredients[idx]}
         onChange={(e) => { this.updateIngredient(e, idx); }}
-        disabled={this.state.readOnly}
       />
     ));
 
@@ -326,44 +238,13 @@ class NewRecipeWidget extends React.Component {
         value={this.state.instructions[idx]}
         onChange={(e) => { this.updateInstruction(e, idx); }}
         rows="2"
-        disabled={this.state.readOnly}
       />
     ));
-
-    // Visibility of the Edit and Save buttons.
-    const { hideEditButton, hideSaveButton } = this.setEditSaveButtonVisibility();
-
-    // Classes to control visibility of form.
-    const classesForm = classNames({
-      'form-newrecipe-widget': true,
-      hidden: this.state.hideForm,
-    });
-
-    // Classes to control visibility of edit and save button.
-    const classesEditButton = classNames({
-      hidden: hideEditButton,
-      'text-center': true,
-    });
-
-    // Classes to control visibility of edit and save button.
-    const classesSaveButton = classNames({
-      hidden: hideSaveButton,
-    });
 
     return (
       <div className="box shadow">
         <AlertBox alert={this.state.alert} handleClose={this.clearAlert} />
-        <form type="POST" className={classesForm} onSubmit={this.handleFormSubmit}>
-
-          <div className={classesEditButton}>
-            <button
-              className="btn btn-submit"
-              type="button"
-              onClick={() => this.setState({ readOnly: !this.state.readOnly })}
-            >
-              { this.state.readOnly ? 'Enable Editing' : 'Disable Editing' }
-            </button>
-          </div>
+        <form type="POST" className="form-newrecipe-widget" onSubmit={this.handleFormSubmit}>
 
           <div className="input-group">
             <div className="text-center">Recipe Title:</div>
@@ -372,7 +253,6 @@ class NewRecipeWidget extends React.Component {
               type="text"
               onChange={e => this.setState({ title: e.target.value })}
               value={this.state.title}
-              disabled={this.state.readOnly}
             />
           </div>
 
@@ -384,7 +264,6 @@ class NewRecipeWidget extends React.Component {
               id="recipe-tagline"
               onChange={e => this.setState({ tagline: e.target.value })}
               value={this.state.tagline}
-              disabled={this.state.readOnly}
             />
           </div>
 
@@ -393,7 +272,6 @@ class NewRecipeWidget extends React.Component {
             <PlusMinus
               handleClick={this.addRemoveFields}
               stateKey="ingredients"
-              readOnly={this.state.readOnly}
             />
             <div className="ingredients-list">{renderIngredients}</div>
           </div>
@@ -403,16 +281,13 @@ class NewRecipeWidget extends React.Component {
             <PlusMinus
               handleClick={this.addRemoveFields}
               stateKey="instructions"
-              readOnly={this.state.readOnly}
             />
             <div className="ingredients-list">{renderInstructions}</div>
           </div>
 
-          <div className={classesSaveButton}>
-            <button className="btn btn-submit width-100" type="submit" value="submit">
-              Save Recipe
-            </button>
-          </div>
+          <button className="btn btn-submit width-100" type="submit" value="submit">
+            Save Recipe
+          </button>
 
         </form>
       </div>
@@ -429,5 +304,4 @@ export default connect(mapStateToProps, null)(withRouter(NewRecipeWidget));
 // Prop validation.
 NewRecipeWidget.propTypes = {
   token: React.PropTypes.string,
-  mode: React.PropTypes.oneOf(['NewRecipe', 'ViewRecipe']),
 };
--- client/containers/NewRecipePage.jsx
+++ client/containers/NewRecipePage.jsx
@@ -10,7 +10,7 @@ const NewRecipePage = () => (
 
     <h1 className="text-center">Create a New Recipe</h1>
 
-    <NewRecipeWidget mode="NewRecipe" />
+    <NewRecipeWidget />
 
   </div>
 );
