19f2d0c7c8fcc7c9c826e4240b3bc8c9ed03f9aa
Jon Balon <development@jonbalon.com>
2017-01-09 10:25:36 -0500

    Removed remaining instances of twitter and passport

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,10 +1,5 @@
 // Module definition.
 const auth = {
-  TWITTER: {
-    CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
-    CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET,
-    CALLBACK_URL: process.env.TWITTER_CALLBACK_URL,
-  },
   KEY: {
     SESSION_SECRET_KEY: process.env.SESSION_SECRET_KEY,
     JWT_SIGNING_KEY: process.env.JWT_SIGNING_KEY,
