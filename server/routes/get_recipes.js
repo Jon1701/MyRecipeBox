6e72b7b79b3ef29d323e3d7c05e115e145d3a8d4
Jon Balon <development@jonbalon.com>
2017-01-11 21:43:21 -0500

    added sort by field functionality, and sort ordering

--- server/routes/get_recipes.js
+++ server/routes/get_recipes.js
@@ -76,11 +76,48 @@ const getRecipes = (req, res, next) => {
     return intPerPage;
   };
 
+  /*
+   *  Helper function to sanitize the sort_order parameter from the query string.
+   *
+   *  If no sort order is provided, -1 is returned. (Sort descending).
+   *  If 'ascending' is provided, 1 is returned. (Sort ascending).
+   */
+  const sanitizeSortOrder = (sortOrder) => {
+    // If the provided sortOrder is 'ascending', return 1.
+    if (sortOrder === 'ascending') {
+      return 1;
+    }
+
+    // Default sort order of 'descending'.
+    return -1;
+  };
+
+  /*
+   *  Helper function to sanitize the sort_by field in the query string.
+   *
+   *  If no field is provided, the date the recipe was created will be used.
+   */
+  const sanitizeSortBy = (sortBy) => {
+    // By default, sort by date recipe was created.
+    const DEFAULT_SORT_FIELD = 'creationDate';
+
+    // Allowed fields to sort by.
+    const ALLOWED_FIELDS = ['username', 'title', 'tagline', 'creationDate'];
+
+    // If the given field is an allowed field, return that field name.
+    if (ALLOWED_FIELDS.indexOf(sortBy) > -1) { return sortBy; }
+
+    // If the given field to sort by is not an allowed field, return default field name.
+    return DEFAULT_SORT_FIELD;
+  };
+
   // Extract request parameters
-  const recipeID = req.query.recipe_id;
-  const username = req.query.username;
-  const pageNum = sanitizePageNum(req.query.pageNum);
-  const perPage = sanitizePerPage(req.query.perPage);
+  const recipeID = req.query.recipe_id; // Recipe ID.
+  const username = req.query.username;  // Recipe author.
+  const pageNum = sanitizePageNum(req.query.pageNum); // Page of results to return.
+  const perPage = sanitizePerPage(req.query.perPage); // Number of results per page to return.
+  const sortOrder = sanitizeSortOrder(req.query.sort_order);  // Sort order of results.
+  const sortBy = sanitizeSortBy(req.query.sort_by);
 
   // Check for invalid page number.
   if (pageNum < 1) { return next(MSG('INVALID_PAGE_NUMBER')); }
@@ -102,25 +139,24 @@ const getRecipes = (req, res, next) => {
   // If username is provided, add it to the search query.
   if (username) { searchQuery.username = username; }
 
-  // Pagination options.
-  const paginationOptions = {
-    skip: (pageNum - 1) * perPage,
-    limit: perPage,
-  };
-
   // Find all matching recipes.
-  Recipe.find(searchQuery, null, paginationOptions, (errFind, resultsFind) => {
-    // Database error handling.
-    if (errFind) { return next(MSG('DB_ERROR')); }
+  Recipe
+    .find(searchQuery, null)
+    .limit(perPage)
+    .skip((pageNum - 1) * perPage)
+    .sort({ [sortBy]: sortOrder })
+    .exec((errFind, resultsFind) => {
+      // Database error handling.
+      if (errFind) { return next(MSG('DB_ERROR')); }
 
-    // Response payload.
-    const payload = {
-      recipes: resultsFind, // List of recipes.
-    };
+      // Response payload.
+      const payload = {
+        recipes: resultsFind, // List of recipes.
+      };
 
-    // Return success response payload.
-    return res.send(Object.assign({}, MSG('RECIPE_SEARCH_COMPLETE'), { payload }));
-  });
+      // Return success response payload.
+      return res.send(Object.assign({}, MSG('RECIPE_SEARCH_COMPLETE'), { payload }));
+    });
 
   // Keep eslint happy.
   return true;
