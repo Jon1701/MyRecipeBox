a95dd97cb55d0093972493a12d5b401e0457dedb
Jon Balon <development@jonbalon.com>
2017-01-20 15:54:44 -0500

    Added Cloudinary API key

--- server/config/auth.js
+++ server/config/auth.js
@@ -4,6 +4,11 @@ const auth = {
     SESSION_SECRET_KEY: process.env.SESSION_SECRET_KEY,
     JWT_SIGNING_KEY: process.env.JWT_SIGNING_KEY,
   },
+  CLOUDINARY: {
+    CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    API_KEY: process.env.CLOUDINARY_API_KEY,
+    API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  },
 };
 
 // Module export.
