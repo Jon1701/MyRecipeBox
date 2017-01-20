c4785bd4ee0b74deb58050e527ba83b396598f5c
Jon Balon <development@jonbalon.com>
2017-01-20 01:52:05 -0500

    Added html5 file input control and callbacks

--- client/components/ImageUploader.jsx
+++ client/components/ImageUploader.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
 
+// Component definition.
 class ImageUploader extends React.Component {
+
+  // Component constructor.
+  constructor(props) {
+    super(props);
+
+    // Bind methods to component instance.
+    this.handleFileChange = this.handleFileChange.bind(this);
+  }
+
+  // Handles change to the file upload control.
+  handleFileChange() {
+    // Get the file.
+    const file = this.fileInput.files[0];
+
+    // FileReader instance.
+    const reader = new FileReader();
+  }
+
   render() {
     return (
       <div className="field">
         <label className="label" htmlFor="recipe-upload">Upload Image</label>
+
+        <input
+          ref={(input) => { this.fileInput = input; }}
+          onChange={() => { this.handleFileChange(); }}
+          className="hidden"
+          id="recipe-upload"
+          type="file"
+          accept="image/bmp,image/jpeg,image/pjpeg,image/png,image/gif"
+        />
+
         <div className="container-inline">
-          <button className="btn btn-default width-100" type="button">Upload Image</button>
-          <button className="btn btn-default width-100" type="button">Remove Image</button>
+          <button
+            onClick={() => this.fileInput.click()}
+            className="btn btn-default width-100"
+            type="button"
+          >
+            Choose Image
+          </button>
+
+          <button
+            className="btn btn-default width-100"
+            type="button"
+          >
+            Remove Image
+          </button>
         </div>
       </div>
     );
