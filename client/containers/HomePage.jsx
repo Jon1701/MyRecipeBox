41bf800260010cd2de8e04c8fedf62cd36f7dd23
Jon Balon <development@jonbalon.com>
2017-01-26 18:21:14 -0500

    Added hero image, content to homepage component

--- client/containers/HomePage.jsx
+++ client/containers/HomePage.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 // Component definition.
 const HomePage = () => (
-  <div className="container-page">
+  <div id="container-homepage" className="text-center">
 
-    <div className="box shadow text-center">
-      <h1>Recipe Box</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+    <div className="container-hero">
+      <h1>My Recipe Box</h1>
+
+      <p>Create and share recipes with friends and family!</p>
+
+      <div>
+        <Link to="/dashboard" className="btn btn-getstarted">Get Started</Link>
+      </div>
     </div>
+
 
   </div>
 );
